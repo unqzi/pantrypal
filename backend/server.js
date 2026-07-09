d6e62c13bc710c1ba05727d9e4b3f2265f6d21b1
unqzi <unqzilele@gmail.com>
2026-07-09 11:54:24 +0200

    setup: load port from .env

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running");
 });
