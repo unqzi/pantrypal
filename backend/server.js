7c4e5220dd4dfae4635e90ac63f48e3e37803493
unqzi <unqzilele@gmail.com>
2026-07-10 12:11:07 +0200

    post to household

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,21 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.post("/households", (req, res) => {
+  const name = req.body.name;
+
+  db.query(
+    "INSERT INTO households (name) VALUES (?)",
+    [name],
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json({ id: result.insertId, name });
+      }
+    },
+  );
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running");
