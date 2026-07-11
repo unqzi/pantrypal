90b8ebed9ef7bddb78a0b0381ae6c0b35289350e
unqzi <unqzilele@gmail.com>
2026-07-11 11:03:56 +0200

    post user route

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
 const db = require("./db");
-
+const bcrypt = require("bcrypt");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.post("/households", (req, res) => {
   const name = req.body.name;
-
   db.query(
     "INSERT INTO households (name) VALUES (?)",
     [name],
@@ -19,6 +18,23 @@ app.post("/households", (req, res) => {
       }
     },
   );
+});
+
+app.post("/users", (req, res) => {
+  const { username, password, household_id } = req.body;
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+    db.query(
+      "INSERT INTO users (username, password, household_id) VALUES (?, ?, ?)",
+      [username, hashedPassword, household_id],
+      (err, result) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.json({ id: result.insertId, username });
+        }
+      },
+    );
+  });
 });
 
 app.listen(process.env.PORT, () => {
